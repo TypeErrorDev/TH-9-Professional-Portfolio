2ecc04ab3b95e56c4b13f961f7b003a19ca873c8
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-06-26 18:10:58 +0300

    UDPATE: fixed HTML validation errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         "body-tw": "#17171F", // super dark almost black // rgb(23, 23, 31) // CMYK (26%, 26%, 0%, 88%)
         "selected-text-tw": "#A3A3FF", // light purple // rgb(163, 163, 255) // CMYK (36%, 36%, 0%, 0%)
-        "theme-tw": "#3F3Fff", // bright dark blue // rgb(63, 63, 255) // CMYK (75%, 75%, 0%, 0%)
+        "theme-tw": "#3F3FFF", // bright dark blue // rgb(63, 63, 255) // CMYK (75%, 75%, 0%, 0%)
         "nav-tw": "#404053", // Dark purple grey // rgb(64, 64, 83) // CMYK (23%, 23%, 0%, 67%)
         "secondary-tw": "#9191A4", // super white grey // rgb(145, 145, 164) // CMYK (12%, 12%, 0%, 36%)
         "badge-tw": "#3F3F51", // Darkish grey // rgb(63, 63, 81) // CMYK (22%, 22%, 0%, 68%)
