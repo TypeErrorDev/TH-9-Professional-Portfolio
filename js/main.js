8aa5770f84533a8bbe7b29268ba3db6174757f4d
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-06-26 14:19:15 +0300

    ADD: two javascript functionality features

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////
+///////////// EVENTLISTENER TO VALIDATE MESSAGE /////////////////
+/////////////////////////////////////////////////////////////////
+
+// Just ensure to use a fake name/email ;)
+
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const validateMessage = document.getElementById("send");
+
+validateMessage.addEventListener("click", (e) => {
+  if (email.value === "" && name.value === "") {
+    e.preventDefault();
+    alert("Please make sure you enter a FAKE name and a FAKE email");
+  } else if (name.value === "") {
+    e.preventDefault();
+    alert("Please make sure you enter a FAKE name");
+  } else if (email.value === "") {
+    e.preventDefault();
+    alert("Please make sure you enter a FAKE email ");
+  } else {
+    alert(
+      `Message function does not quite work yet, Please use my social media to contact me`
+    );
+  }
+});
+
+/////////////////////////////////////////////////////////////////
+///////////// EVENTLISTENER TO ADD HIDDEN CLASS /////////////////
+/////////////////////////////////////////////////////////////////
+
+/* Toggle between showing and hiding the navigation menu links when 
+the user clicks on the hamburger menu / bar icon */
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+menu.addEventListener("click", (e) => {
+  if (e.target === btn && e.target === menu) {
+    menu.classList.add("hidden");
+  }
+});
