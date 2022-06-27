fb3811a705f248b6dd2b5f7273f06f1c374e718d
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-06-27 04:02:36 +0300

    DELETE: nav-dot comment code blocks

--- js/main.js
+++ js/main.js
@@ -45,31 +45,3 @@ menu.addEventListener("click", (e) => {
     menu.classList.add("hidden");
   }
 });
-
-/////////////////////////////////////////////////////////////////
-/////////////////// NAV DOT FUNCTIONALITY ///////////////////////
-/////////////////////////////////////////////////////////////////
-
-// function updateList() {
-//   const titles = [...document.querySelectorAll("h1, h2")].sort((a, b) => {
-//     return (
-//       Math.abs(a.getBoundingClientRect().top) -
-//       Math.abs(b.getBoundingClientRect().top)
-//     );
-//   });
-
-//   document
-//     .querySelectorAll(".selected-circle")
-//     .forEach((c) => c.classList.remove("selected-circle"));
-
-//   document
-//     .querySelectorAll(".nav-dot")
-//     [[...document.querySelectorAll("h1, h2")].indexOf(titles[0])].classList.add(
-//       "selected-circle"
-//     );
-// }
-
-// updateList();
-// window.addEventListener("scroll", () => {
-//   updateList();
-// });
